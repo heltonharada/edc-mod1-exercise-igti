81c056abcc01eb78639e6371f244ef991ee2b6f9
Helton <emaildohelton@gmail.com>
2022-03-02 15:26:11 -0300

    modificado para declaração como recursos independentes, conforme nova versao do Terraform

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,18 +1,38 @@
 resource "aws_s3_bucket" "dl" {
   # parâmetros de configuração do recurso escolhido
   bucket = "datalake-helton-igti-edc-tf"
-  acl    = "private"
+  # acl    = "private" (atualizando forma de declarar recursos conforme nova versão do Terraform:
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-4-upgrade)
 
   tags = {
     IES   = "IGTI",
     CURSO = "EDC"
   }
 
-  server_side_encryption_configuration {
-    rule {
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }  
+}
+
+
+# atualizando forma de declarar recursos conforme nova versão do Terraform:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-4-upgrade)
+
+resource "aws_s3_bucket_acl" "dl" {
+  bucket = aws_s3_bucket.dl.id
+  status = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "dl" {
+  bucket = aws_s3_bucket.dl.id
+
+  rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
-    }
   }  
 }
--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,5 +1,11 @@
-resource "aws_s3_bucket_object" "job_spark" {
-  bucket = aws_s3_bucket.datalake.id
+# atualizando forma de declarar recursos conforme nova versão do Terraform:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/version-4-upgrade)
+
+# resource "aws_s3_bucket_object" "job_spark" {
+resource "aws_s3_object" "job_spark" {
+  # bucket = aws_s3_bucket.datalake.id
+  bucket = aws_s3_bucket.dl.id
+
   key    = "emr-code/pyspark/job_spark_from_tf.py"
   acl    = "private"
   source = "../job_spark.py"
