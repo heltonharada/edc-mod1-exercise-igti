722b493d2682f98c63dc4caa2bd5510516c5ef26
Helton <emaildohelton@gmail.com>
2022-03-09 12:40:56 -0300

    arquivos crus realocados para pastas no s3 (enem)

--- interact_s3.py
+++ interact_s3.py
@@ -5,7 +5,7 @@ import os
 #criar um cliente para interagir com o AWS S3
 s3_client = boto3.client('s3')
 
-s3_client.upload_file('data/DADOS/MICRODADOS_ENEM_2020.csv', 'datalake-helton-739010676765', 'raw-data/ENEM_2020.csv')
+s3_client.upload_file('data/DADOS/MICRODADOS_ENEM_2020.csv', 'datalake-helton-739010676765', 'raw-data/enem/ENEM_2020.csv')
 
 
 # s3_client.download_file('datalake-helton-739010676765', 'raw-data/ITENS_PROVA_2020.csv', 'ITENS_PROVA_2020.csv')
